4b5ecfffaa3a856665ecd07a493abbfc3d24527f
Luis Perez <luiggiz@hotmail.com>
2012-11-10 13:46:45 -0600

    asdasd

--- js/action.js
+++ js/action.js
@@ -12,7 +12,23 @@ function docReady(){
 		//Eliminamos campo
 		window.localStorage.removeItem("active");
 		//limpiando
-		window.localStorage.clear();*/	
+		window.localStorage.clear();*/
+		function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+};
+
+function onError() {
+    alert('onError!');
+};
+
+var options = { frequency: 3000 };  // Update every 3 seconds
+
+var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+		
+			
 			
 		if (window.localStorage.getItem("active")){
 			alert(window.localStorage.getItem("active"));
