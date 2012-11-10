4b145e14a45b24a2c582ecbad9ce740868d8e311
Luis Perez <luiggiz@hotmail.com>
2012-11-10 14:03:06 -0600

    asd

--- js/action.js
+++ js/action.js
@@ -14,10 +14,10 @@ function docReady(){
 		//limpiando
 		window.localStorage.clear();*/
 		function onSuccess(acceleration) {
-    alert('Acceleration X: ' + acceleration.x + '\n' +
-          'Acceleration Y: ' + acceleration.y + '\n' +
-          'Acceleration Z: ' + acceleration.z + '\n' +
-          'Timestamp: '      + acceleration.timestamp + '\n');
+          xvi.innerHTML = acceleration.x ;
+          yvi.innerHTML = acceleration.y ;
+          zvi.innerHTML = acceleration.z ;
+          tvi.innerHTML = acceleration.timestamp;
 };
 
 function onError() {
